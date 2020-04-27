3fb4426341dca74b27b8c7009ad6123e959fbb3f
juliandavis7 <47072603+juliandavis7@users.noreply.github.com>
2020-04-27 10:54:28 -0700

    sonar cloud 17

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 
+import java.lang.*;
+
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World!");
    }
 }
-
